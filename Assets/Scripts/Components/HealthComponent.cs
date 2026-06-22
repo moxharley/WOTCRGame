71e796570541958145c05aae7cd60c365ffb267c
Al0102 <alu59@my.bcit.ca>
2026-06-22 02:17:05 -0700

    Add wizard with health and inventory

---
+++ Assets/Scripts/Components/HealthComponent.cs
@@ -0,0 +1,70 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Components
+{
+    public class HealthComponent : MonoBehaviour
+    {
+        private const double DefaultHitPoints = 100;
+
+        [SerializeField, Min(0)] private double maxHitPoints = DefaultHitPoints;
+        [SerializeField] private double currentHitPoints = DefaultHitPoints;
+
+        public double MaxHitPoints
+        {
+            get => maxHitPoints;
+            set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentOutOfRangeException("Expected: maxHitPoints >= 0; received " + value);
+                }
+
+                maxHitPoints = value;
+            }
+        }
+
+        public double CurrentHitPoints
+        {
+            get => currentHitPoints;
+            set => currentHitPoints = Math.Min(MaxHitPoints, value);
+        }
+
+        public bool IsAlive => currentHitPoints > 0;
+        public bool IsDead => !IsAlive;
+
+        public void Hurt(double amount)
+        {
+            if (amount < 0)
+            {
+                throw new ArgumentOutOfRangeException("Expected: amount >= 0; received " + amount);
+            }
+
+            currentHitPoints -= amount;
+        }
+
+        public void Heal(double amount)
+        {
+            if (amount < 0)
+            {
+                throw new ArgumentOutOfRangeException("Expected: amount >= 0; received " + amount);
+            }
+
+            currentHitPoints = Math.Min(MaxHitPoints, amount);
+        }
+
+        public void MaxHeal()
+        {
+            currentHitPoints = MaxHitPoints;
+        }
+
+        private void OnValidate()
+        {
+            if (currentHitPoints > maxHitPoints)
+            {
+                currentHitPoints = maxHitPoints;
+            }
+        }
+    }
+}
--- Assets/Scripts/Wizard.cs
+++ Assets/Scripts/Wizard.cs
@@ -1,16 +1,9 @@
 using UnityEngine;
+using System.Collections.Generic;
+using Components;
 
 public class Wizard : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    [SerializeField] private HealthComponent healthComponent;
+    [SerializeField] private InventoryComponent inventoryComponent;
 }
