bfc9cb8cb25d8880107e2cd71b5890fd0a291c81
Harlan Bullock <harlanbull7@gmail.com>
2026-06-25 14:14:10 -0700

    Add stat components

---
+++ Assets/Scripts/Components/SpellHealComponent.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class SpellHealComponent : MonoBehaviour
+{
+    private const double DefaultSpellHeal = 100;
+
+    [Header("Spell Heal Info")]
+    [SerializeField, Min(0)] private double spellHealValue = DefaultSpellHeal;
+
+    public void IncreaseHealValue(double value)
+    {
+        if (value > 0) spellHealValue += value;
+    }
+
+    public void DecreaseHealValue(double value)
+    {
+        if (value > 0) spellHealValue -= value;
+        if (spellHealValue < 1) spellHealValue = 1;
+    }
+
+    public double GetSpellHealValue()
+    {
+        return spellHealValue;
+    }
+}
